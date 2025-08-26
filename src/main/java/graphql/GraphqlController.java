d307d0cd0894ef3d8e3fe664025673a109b3313c
jakdabrowski <jakdabrowski@inpost.pl>
2025-08-26 17:00:16 +0200

    first commit

---
+++ src/main/java/graphql/GraphqlController.java
@@ -0,0 +1,26 @@
+package graphql;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GraphqlController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
